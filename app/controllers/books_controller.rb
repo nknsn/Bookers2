62870070ce635d8e4c67a420c5e77a7febd96d59
EC2 Default User <ec2-user@ip-172-31-0-22.ec2.internal>
2022-02-21 10:19:00 +0000

    [Add]部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path(@book)
+      redirect_to user_path(current_user)
       flash[:notice]='You have created book successfully.'
     else
       @books = Book.all
